4c404168bc8b12f92f6482312bda9f6d695f12da
funnythingz <hiroki@hackleberry.me>
2014-04-03 23:41:07 +0900

    Fixed dir

---
+++ src/ts/rackle/view.ts
@@ -0,0 +1,17 @@
+/// <reference path="../../../definitions/jquery.d.ts" />
+/// <reference path="../../../definitions/handlebars.d.ts" />
+
+module RACKLE {
+
+    interface IViewCreateOptions {
+        $el?: JQuery;
+        tagName?: string;
+        id?: string;
+        className?: string;
+        attributes?: Object;
+    }
+
+    export class View {
+    }
+
+}
